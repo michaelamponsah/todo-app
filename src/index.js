0e52097f694984bc17324b2082513dafb6323398
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-12-06 18:22:13 +0000

    Add array of task objects

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+
+const data = [
+
+  {
+    index: 1,
+    description: 'do my laundry',
+    isCompleted: false,
+  },
+  {
+    index: 2,
+    description: 'build a new feature for fitness app',
+    isCompleted: false,
+  }
+]
