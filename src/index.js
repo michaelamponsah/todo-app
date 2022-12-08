7eee31369714c41c371d12077ae741ca2cb38055
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-12-08 06:36:23 +0000

    Refactor logic for adding todo item to list

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@ import 'lodash';
 import './style.css';
 
 import todoListBuilder from './modules/todoListBuilder.js';
+import saveResourceToLocalStorage from './modules/saveResourceToLocalStorage.js';
+import getResourceFromLocalStorage from './modules/getResourceFromLocalStorage.js';
 
 const todoListWrapper = document.querySelector('[data-list-wrapper]');
 const todoInputField = document.querySelector('[data-todo-input]');
-const addBtn = document.querySelector('[data-add-btn]');
+// const addBtn = document.querySelector('[data-add-btn]');
 
-const data = [];
+const dataFromStorage = getResourceFromLocalStorage('todos');
 
-const renderData = () => {
+const data = dataFromStorage ? [...dataFromStorage] : [];
+
+const renderData = (data) => {
+  if (!data) return;
+
   data.forEach((listItem) => {
     todoListWrapper.innerHTML += todoListBuilder(listItem);
   });
 };
 
-window.onload = renderData;
+renderData(data);
 
 todoInputField.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
@@ -24,7 +30,20 @@ todoInputField.addEventListener('keypress', (e) => {
       description: e.target.value,
       isCompleted: false,
     };
-    data.push(todoObject);
+
     e.target.value = '';
+
+    if (!dataFromStorage) {
+      data.push(todoObject);
+      saveResourceToLocalStorage('todos', data);
+      window.location.reload();
+      return true;
+    }
+
+    const newEntry = [...data];
+    newEntry.push(todoObject);
+    saveResourceToLocalStorage('todos', newEntry);
+    window.location.reload();
   }
+  return true;
 });
