396af659f55497a0dca22ec4cf9c2d7d982d166f
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-12-10 07:40:25 +0000

    Modify listBuilderLogic to include item check attribute

--- src/modules/todoListBuilder.js
+++ src/modules/todoListBuilder.js
@@ -5,7 +5,7 @@ const todoListBuilder = (data) => {
       <div class="todo-list--item padding">
         <div class="input-desc-wrapper">
           <input type="checkbox" data-inputcheck ${isCompleted ? 'checked' : ''}>
-          <input type="text" class="description" value="${description}"></input>
+          <input type="text" class="description" value="${description}" ${isCompleted ? 'readonly="true"' : ''}></input>
         </div>
         <i class="fa-solid fa-trash icon" data-delete></i>
         </div>
