d4087b8b24e7be191212281ca6dbddae1e4ef90c
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-12-11 23:52:36 +0000

    Implement logic to add list item to markup

---
+++ src/modules/addToMarkup.js
@@ -0,0 +1,18 @@
+import todoListBuilder from './todoListBuilder.js';
+
+const addToMarkup = (data, location) => {
+  if (!data) return;
+
+  // location.innerHTML = '';
+  const li = document.createElement('li');
+  li.classList.add('todo-list-item--wrapper');
+  li.innerHTML = todoListBuilder(data);
+  location.insertAdjacentElement('beforeend', li);
+
+  // data.forEach((listItem) => {
+  //   li.innerHTML += todoListBuilder(listItem);
+  //   location.insertAdjacentElement('beforeend', li);
+  // });
+};
+
+export default addToMarkup;
