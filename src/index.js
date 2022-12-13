2ea8212d9035fac65d1d12d6e27d7fe65fff0646
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-12-13 14:11:20 +0000

    Refactor logic for todo complete status
    Put change event listener on parent ul to make use of event delegtion

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './style.css';
 
 import getResourceFromLocalStorage from './modules/getResourceFromLocalStorage.js';
 import saveTodo from './modules/saveTodo.js';
-// import renderData from './modules/addToMarkup.js';
 import handleItemFocus from './modules/handleItemFocus.js';
 import updateTodo from './modules/updateTodo.js';
 import handleItemDelete from './modules/handleItemDelete.js';
@@ -12,10 +11,10 @@ import handleClearAll from './modules/handleClearAll.js';
 import loadToDos from './modules/loadToDos.js';
 
 const todoInputField = document.querySelector('[data-todo-input]');
+const todoListWrapper = document.querySelector('[data-list-wrapper]');
 
 // Get data from local storage
-const dataFromStorage = getResourceFromLocalStorage('todos');
-const todosArray = dataFromStorage || [];
+const todosArray = getResourceFromLocalStorage('todos') || [];
 
 // Render data when page loads
 window.onload = loadToDos(todosArray);
@@ -31,7 +30,6 @@ todoInputField.addEventListener('keypress', (e) => {
 
     e.target.value = '';
     saveTodo(newTodo);
-    // window.location.reload();
   }
 
   return true;
@@ -40,7 +38,7 @@ todoInputField.addEventListener('keypress', (e) => {
 const textEntries = document.querySelectorAll('.description');
 const deleteIcons = document.querySelectorAll('[data-delete]');
 
-// Handle focus (background color) of active todo items
+// Handle focus (background color & delete icon handler) of active todo items
 handleItemFocus(textEntries, deleteIcons);
 
 // Update todo items
@@ -72,11 +70,16 @@ textEntries.forEach((textEntry) => {
   });
 });
 
-// Delete todo item
-deleteIcons.forEach((icon, index) => {
-  icon.addEventListener('click', () => {
-    handleItemDelete(index);
-  });
+// Handle todo item delete
+todoListWrapper.addEventListener('click', (e) => {
+  const todoId = e.target.parentNode.parentNode.getAttribute('id');
+  const isdeleteIcon = e.target.matches('[data-delete]');
+
+  if (isdeleteIcon) {
+    const index = Number.parseInt(todoId.slice(5), 10);
+    document.getElementById(todoId).remove();
+    handleItemDelete((index - 1));
+  }
 });
 
 // Handle page reload
@@ -85,12 +88,17 @@ document.querySelector('[data-refresh]').addEventListener('click', () => {
 });
 
 // Handle todo complete status
-document.querySelectorAll('[data-inputcheck]').forEach((inputCheck, index) => {
-  inputCheck.addEventListener('change', (e) => {
+todoListWrapper.addEventListener('change', (e) => {
+  const isCheckInput = e.target.matches('[data-inputcheck]');
+  const todoId = e.target.closest('li').getAttribute('id');
+  const textEntry = e.target.nextElementSibling;
+  const index = Number.parseInt(todoId.slice(5), 10);
+
+  if (isCheckInput) {
     const isCompleted = e.target.toggleAttribute('checked');
-    textEntries[index].toggleAttribute('data-task-complete');
-    handleTaskComplete(index, isCompleted);
-  });
+    textEntry.toggleAttribute('data-task-complete');
+    handleTaskComplete((index - 1), isCompleted);
+  }
 });
 
 // Handle clear all
--- src/modules/handleTaskComplete.js
+++ src/modules/handleTaskComplete.js
@@ -1,16 +1,11 @@
 import getResourceFromLocalStorage from './getResourceFromLocalStorage.js';
-import renderData from './renderData.js';
 import saveResourceToLocalStorage from './saveResourceToLocalStorage.js';
 
 const todos = getResourceFromLocalStorage('todos');
-const todoListWrapper = document.querySelector('[data-list-wrapper]');
 
 const handleTaskComplete = (index, isCompleted) => {
   todos[index].isCompleted = isCompleted;
   saveResourceToLocalStorage('todos', todos);
-  renderData(todos, todoListWrapper);
-
-  window.location.reload();
 };
 
 export default handleTaskComplete;
