95440e1f7ce3309cf252c27a2ab5c8fe661b1fa0
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-12-13 09:27:15 +0000

    Remove commented and unused code

--- src/modules/addToMarkup.js
+++ src/modules/addToMarkup.js
@@ -2,19 +2,12 @@ import todoListBuilder from './todoListBuilder.js';
 
 const addToMarkup = (data, location) => {
   if (!data) return;
-
-  // location.innerHTML = '';
   const li = document.createElement('li');
   li.classList.add('todo-list-item--wrapper');
   li.setAttribute('id', `todo-${data.index}`);
 
   li.innerHTML = todoListBuilder(data);
   location.insertAdjacentElement('beforeend', li);
-
-  // data.forEach((listItem) => {
-  //   li.innerHTML += todoListBuilder(listItem);
-  //   location.insertAdjacentElement('beforeend', li);
-  // });
 };
 
 export default addToMarkup;
